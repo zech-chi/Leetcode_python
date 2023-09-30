8ef35d0a4278885e2dc74440e9ab74e075cec3c9
zakaria0101echifaouy <machinelearning1.618@gmail.com>
2023-09-30 15:29:38 +0100

    add solution.py

--- 1 easy/171. Excel Sheet Column Number/solution.c
+++
@@ -1,9 +0,0 @@
-int titleToNumber(char * columnTitle){
-    int res = 0;
-    char *s = columnTitle;
-    while (*s){
-        res = res * 26 + (*s - 'A') + 1;
-        s++; 
-    }
-    return (res);
-}
